45a15a8814299c41d8388870e9c9940fb4418598
Rodrigo Pinheiro Matias <rodrigopmatias@gmail.com>
2016-06-15 10:46:28 -0300

    added clean for volumes

--- docker.sh
+++ docker.sh
@@ -2,4 +2,5 @@
 function docker_clear() {
     docker rmi -f $(docker images | grep -i '^<none>' | awk {'print $3'})
     docker rm -f $(docker ps -a | awk {'print $1'})
+    docker volume rm $(docker volume ls | awk {'print $2'})
 }
