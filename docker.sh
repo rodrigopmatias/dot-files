8e05c6963f449a6300160ba0ee200ce3fd700230
Rodrigo Pinheiro Matias <rodrigopmatias@gmail.com>
2018-04-03 11:12:39 -0300

    shotcuts for docker service

--- docker.sh
+++ docker.sh
@@ -1,6 +1,20 @@
 
-function docker_clear() {
+function dkClear() {
     docker rmi -f $(docker images | grep -i '^<none>' | awk {'print $3'})
     docker rm -f $(docker ps -a | awk {'print $1'})
     docker volume rm $(docker volume ls | awk {'print $2'})
+}
+
+function dksContainer() {
+    echo $(docker ps -f "name=$1" --format "{{.ID}}") | cut -d ' ' -f 1
+}
+
+function  dksExec() {
+  name=$1
+  shift 1
+  docker exec -it $(dksContainer $name) $@
+}
+
+function dksTailf() {
+  docker service logs -f --since 0s $1
 }
